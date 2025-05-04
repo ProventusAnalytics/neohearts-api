62e640cd54050c12097d61d8d4dd0daad797544a
Pratyush3930 <ozprat3@gmail.com>
2025-05-04 21:02:01 +0545

    Updated the frontend url

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:5173")
+        builder.WithOrigins("https://main.d2m38mqvlgl9jn.amplifyapp.com")
                .AllowAnyMethod()
                .AllowAnyHeader();
     });
